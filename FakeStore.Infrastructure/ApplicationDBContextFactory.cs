efa69f9114ba82e62835e9e7c5a0e68b0fb5272b
Abdullah Alabd <Abdullah_Alabd@outlook.com>
2023-05-04 00:49:46 +0300

    Add: Implemented ApplicationDBContextFactory and  added correspoding appsettings file for the connection string.

---
+++ FakeStore.Infrastructure/ApplicationDBContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace FakeStore.DataAccess
+{
+    public class ApplicationDBContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
+    {
+        public ApplicationDBContext CreateDbContext(string[] args)
+        {
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+
+            return new ApplicationDBContext(optionsBuilder.Options);
+        }
+    }
+}
