11cfd67241cb35a5b8a814a269c400ed1d6e8c1d
Abdullah Alabd <Abdullah_Alabd@outlook.com>
2023-05-03 21:26:43 +0300

    Add: Initial version of UserDAO

---
+++ FakeStore.Infrastructure/DAO/Concrete/UserDAO.cs
@@ -0,0 +1,24 @@
+﻿using FakeStore.DataAccess.DAO.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeStore.DataAccess.DAO.Concrete
+{
+    public class UserDAO : IUserDAO
+    {
+        private readonly ApplicationDBContext _context;
+        public UserDAO(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddUser(string username, string password)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FakeStore.Infrastructure/DAO/Interface/IUserDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeStore.DataAccess.DAO.Interface
+{
+    public interface IUserDAO 
+    {
+        public Task AddUser(string username, string password);
+    }
+}
