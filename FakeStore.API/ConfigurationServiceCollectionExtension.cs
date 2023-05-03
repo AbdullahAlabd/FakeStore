ea8e54015879e7c62bbee9dc6e621a0ab21c9fdf
Abdullah Alabd <Abdullah_Alabd@outlook.com>
2023-05-03 21:24:17 +0300

    Update: removed dummy weather endpoint and Added App Collection Extention Method

---
+++ FakeStore.API/ConfigurationServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using FakeStore.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeStore.API
+{
+    public static class ConfigurationServiceCollectionExtension
+    {
+        public static void AppCollectionExtension(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region resolve the context 
+            services.AddDbContext<ApplicationDBContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            #endregion            
+        }
+    }
+}
--- FakeStore.API/Program.cs
+++ FakeStore.API/Program.cs
@@ -13,6 +13,9 @@ namespace FakeStore.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Use Dependency Injection Extention
+            builder.Services.AppCollectionExtension(builder.Configuration);
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,28 +26,7 @@ namespace FakeStore.API
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-            var summaries = new[]
-            {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateTime.Now.AddDays(index),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
             app.Run();
         }
     }
