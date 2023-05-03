87ce9ca4cb810b9f5fbaed17ca5449c5b732a3a4
Abdullah Alabd <Abdullah_Alabd@outlook.com>
2023-05-03 21:22:56 +0300

    Refactor: moved entity models to Data Access project and renamed Infrastructure to Data Access and Renamed Core project to Bussiness

---
+++ FakeStore.Core/EntityManagers/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeStore.DataAccess;
+using FakeStore.DataAccess.DAO.Interface;
+
+namespace FakeStore.Business.EntityManagers
+{
+    public class UserManager
+    {
+        private readonly IUserDAO _userDAO;
+        public UserManager(IUserDAO userDAO)
+        {
+            _userDAO = userDAO;
+        }
+        public async Task AddUser()
+        {
+            // TODO: implement
+            _userDAO.AddUser("Dummy", "123");
+        }
+    }
+}
